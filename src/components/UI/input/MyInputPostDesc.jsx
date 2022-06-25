9bf4dde421b8bfd8f0f2a495d30bd5e4245812cd
vmosenkov <vladmos1995@gmail.com>
2022-06-25 10:02:50 +0300

    Create MyInputPostDesc.jsx

---
+++ src/components/UI/input/MyInputPostDesc.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+//import classes from './MyInput.module.css';
+import styled from 'styled-components';
+import { MdSubtitles } from 'react-icons/md'
+
+const InputContainer = styled.label`
+	background-color: var(--color-ui-base);
+	padding: 1rem 2rem;
+	display: flex;
+	align-items: center;
+	border-radius: var(--radius);
+	box-shadow: var(--shadow);
+	width: 100%;
+	margin-bottom: 1.5rem;
+
+/*   @media (min-width: 767px) {
+   margin-bottom: 0rem;
+   width: 300px;
+
+  @media (max-width: 767px) {
+   margin-bottom: 1.5rem;
+   width: 300px; */
+ }
+`;
+
+const InputPostDesc = styled.input.attrs({
+	type: 'text',
+	placeholder: 'Описание поста'
+})`
+	width: 100%; //размер поля текста
+	margin-left: 2rem;
+	border: none;
+	outline: none;
+	background-color: var(--color-ui-base);
+	color: var(--color-text);
+`;
+
+
+
+const MyInputPostDesc = (props) => {
+	
+  	return (
+		
+		<InputContainer>
+		<MdSubtitles/>
+    		<InputPostDesc {...props}/>
+		</InputContainer>
+  	)
+}
+
+export default MyInputPostDesc;
