9055dd6f6b96a767f74a1434348ade3a0d4ad4dc
vmosenkov <vladmos1995@gmail.com>
2022-07-29 22:19:46 +0300

    Update MyButton.jsx

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-//import classes from './MyButton.module.css';
-import styled from 'styled-components';
-
-export const Button = styled.button`
-  padding: 0 1rem;
-  background-color: var(--color-ui-base);
-  box-shadow: var(--shadow);
-  line-height: 2.5;
-  border-radius: var(--radius);
-  border: none;
-  display: flex;
-  align-items: center;
-  gap: 0.75rem;
-  color: var(--color-text);
-  cursor: pointer;
-`;
-
+import cl from './MyButton.module.css';
 
 const MyButton = ({children, ...props}) => {
 	
 	return (
-    	<Button {...props}>{children}</Button>
+    	<button {...props} className={cl.myBtn}>{children}</button>
   	)
 }
 
