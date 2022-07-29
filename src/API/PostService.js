c1bd6c8aa0e81a24ac10ebb5aab839fcbc97b40d
vmosenkov <vladmos1995@gmail.com>
2022-07-29 22:18:56 +0300

    Update PostService.js

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
 export default class PostService {
- static async getAll(limit = 10, page = 1) {
+	static async getAll(
+		limit = 10, 
+		page = 1
+		) {
 		const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
 			params: {
 				_limit: limit,
 				_page: page,
 			}
 		})
-  	return response;
- }
+  		return response;
+	}
 }
 
 
