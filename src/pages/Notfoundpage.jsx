a5240181309e266a1b6190fce376f95c6ec943b8
vmosenkov <vladmos1995@gmail.com>
2022-07-29 22:20:22 +0300

    Create Notfoundpage.jsx

---
+++ src/pages/Notfoundpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfoundpage = () => {
+	return (
+		<div className="container">
+			<h1>Страница не найдена</h1>
+		</div>
+	);
+}
+
+export default Notfoundpage;
