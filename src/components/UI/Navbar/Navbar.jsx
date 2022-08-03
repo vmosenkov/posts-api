2d8cc1ff7ee5b45b66c27e81d034026e91d54708
vmosenkov <vladmos1995@gmail.com>
2022-08-03 11:41:43 +0300

    Update Navbar.jsx

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useContext} from 'react';
 import { useState, useEffect } from "react";
 import { IoMoonOutline, IoMoon } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../../context';
 
 import cl from './Navbar.module.css';
+import MyButton from '../button/MyButton';
 
 
 export const Navbar = () => {
@@ -16,6 +17,14 @@ export const Navbar = () => {
     document.body.setAttribute('data-theme', theme);
   }, [theme]);
 
+	const { isAuth, setIsAuth } = useContext(AuthContext);
+
+
+	const logout = () => {
+		setIsAuth(false);
+		localStorage.removeItem('auth')
+	}
+
 	return (
 		<div className="container">
 			<div className={cl.wrapper}>
@@ -24,6 +33,12 @@ export const Navbar = () => {
 					<Link className={cl.navbar__links} to="/about">О сайте</Link>
 					<Link className={cl.navbar__links} to="/posts">Посты</Link>
 				</div>
+				<div>
+					<Link className={cl.navbar__links} to="/login">Логин</Link>
+				</div>
+					<MyButton onClick={logout}>
+						Выйти
+					</MyButton>
 				<div className={cl.themeSwitcher} onClick={toggleTheme}>
 					{theme === 'light' ? (
 						<IoMoonOutline size="14px" />
