4dfaf67762e41db161b0520395beae8c5abbe91f
vmosenkov <vladmos1995@gmail.com>
2022-08-03 11:41:19 +0300

    Create PostList.jsx

---
+++ src/components/Post/PostList/PostList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import PostItem  from '../PostItem/PostItem';
+
+const PostList = ({posts, title, remove}) => {
+
+	if (!posts.length) {
+		return (
+			<h1 style={{textAlign: 'center', marginTop: '20%'}}>
+				Хей, дружок-пирожок! Постов нет, но ты можешь его создать сам.
+			</h1>
+		)
+	}
+
+	return (
+	<div>
+		<h1 style={{textAlign: 'center'}}>
+			{title}
+		</h1>
+		<TransitionGroup>
+			{posts.map((post, index) =>
+			<CSSTransition 
+				key={post.id}
+				timeout={500}
+				classNames="post"
+			>
+				<PostItem remove={remove} number={index + 1} post={post}/>
+			</CSSTransition>
+		)}
+		</TransitionGroup>
+	</div>
+	)
+}
+
+export default PostList;
