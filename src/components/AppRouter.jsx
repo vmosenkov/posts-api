2e8fd74dd3608cfc22a39db6fda7cb58356efd03
vmosenkov <vladmos1995@gmail.com>
2022-08-03 11:41:04 +0300

    Update AppRouter.jsx

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-
-/* Page */
-import Homepage from '../pages/Homepage';
-import Aboutpage from '../pages/Aboutpage';
-import Posts from '../pages/Posts';
-import Notfoundpage from '../pages/Notfoundpage';
+import React, {useContext}  from 'react';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { publicRoutes, privateRoutes } from '../router/route';
+import {AuthContext} from "../context";
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
+	const {isAuth, isLoading} = useContext(AuthContext);
+
+	if (isLoading) {
+		return <Loader/>
+	}
+
 	return (
-		<Routes>
-			<Route path="/" element={<Homepage />} />
-			<Route path="/about" element={<Aboutpage />} />
-			<Route path="/posts" element={<Posts />} />
-			<Route path="*" element={<Notfoundpage />} />
-		</Routes>
+		isAuth 
+			?
+			<Routes>
+				{privateRoutes.map(route =>
+					<Route 
+						path={route.path}
+						element={<route.element />}
+						key = {route.path} 
+					/>
+				)}
+			</Routes>
+			:
+			<Routes>
+				{publicRoutes.map(route =>
+					<Route 
+						path={route.path}
+						element={<route.element />}
+						key = {route.path} 
+					/>
+				)}
+				<Route path="/*" element={<Navigate to="/login" replace />} />
+			</Routes>
 	);
 }
 
 export default AppRouter;
+
+
+
+
+
+
+
+{/* <Routes>
+	<Route path="/" element={<Homepage />} />
+	<Route path="/about" element={<Aboutpage />} />
+	<Route path="/posts" element={<Posts />} />
+	<Route path="/posts/:id" element={<PostIdPage />} />
+	<Route path="*" element={<Notfoundpage />} />
+</Routes> */}
