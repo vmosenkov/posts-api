8442fd245b5d73778b8a22c9f64240ade6eafbd5
vmosenkov <vladmos1995@gmail.com>
2022-08-04 16:38:19 +0300

    Updates

---
+++ precache-manifest.a06f4e78b49cd7729d57ca676b91ddf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1e55cc8f182e97b7c1a",
+    "url": "/posts-api/static/css/main.8857033b.chunk.css"
+  },
+  {
+    "revision": "f1e55cc8f182e97b7c1a",
+    "url": "/posts-api/static/js/main.f1e55cc8.chunk.js"
+  },
+  {
+    "revision": "921c5787a73fa97a64d7",
+    "url": "/posts-api/static/js/1.921c5787.chunk.js"
+  },
+  {
+    "revision": "e8ed1065eba2b526a81d",
+    "url": "/posts-api/static/js/runtime~main.e8ed1065.js"
+  },
+  {
+    "revision": "0063d7b848f49175ed05d086a7fda8b0",
+    "url": "/posts-api/index.html"
+  }
+];
